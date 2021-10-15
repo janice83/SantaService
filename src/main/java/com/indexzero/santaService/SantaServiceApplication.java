5c229a2b23abb9e0890df447a9f271ae5d0dda55
SJarno <jarno.saastamoinen@hotmail.com>
2021-10-15 12:41:33 +0300

    initial commit

---
+++ src/main/java/com/indexzero/santaService/SantaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.indexzero.santaService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indexzero/santaService/SantaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indexzero.santaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
