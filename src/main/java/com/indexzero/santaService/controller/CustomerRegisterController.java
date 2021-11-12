5f3f61e1873d25db78544839e367dcfd05e0bf0b
SJarno <jarno.saastamoinen@hotmail.com>
2021-11-12 12:09:26 +0200

    add santa and customer pages

--- src/main/java/com/indexzero/santaService/controller/CustomerRegisterController.java
+++ src/main/java/com/indexzero/santaService/controller/CustomerRegisterController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DefaultController {
+public class CustomerRegisterController {
 
-    @GetMapping("/")
+    @GetMapping("/customer-register")
     public String home() {
-        return "index";
+        return "customer-registration";
     }
     
 }
--- src/main/java/com/indexzero/santaService/controller/SantaRegisterController.java
+++ src/main/java/com/indexzero/santaService/controller/SantaRegisterController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DefaultController {
+public class SantaRegisterController {
 
-    @GetMapping("/")
+    @GetMapping("/santa-register")
     public String home() {
-        return "index";
+        return "santa-registration";
     }
     
 }
