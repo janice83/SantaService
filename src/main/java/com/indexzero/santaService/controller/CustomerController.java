3dab930bc81019756765ed39a41b20a907886ea7
SJarno <jarno.saastamoinen@hotmail.com>
2021-12-03 11:08:01 +0200

    fix issue with santa and customer reg

--- src/main/java/com/indexzero/santaService/controller/CustomerController.java
+++ src/main/java/com/indexzero/santaService/controller/CustomerController.java
@@ -1,16 +1,39 @@
 package com.indexzero.santaService.controller;
 
+import com.indexzero.santaService.model.UserAccount;
+import com.indexzero.santaService.services.UserAccountService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/customer")
+
 @Controller
 public class CustomerController {
 
-    @GetMapping("")
+    @Autowired
+    private UserAccountService userAccountService;
+
+    @ModelAttribute
+    private UserAccount getCustomer() {
+        return new UserAccount();
+    }
+
+    @GetMapping("/customer")
     public String getCustomerPage() {
         return "customer";
+    }
+    @PostMapping("/customer-register")
+    public String register(@ModelAttribute UserAccount customerAccount) {
+        System.out.println();
+        System.out.println("Luodaan tiliä");
+        System.out.println(customerAccount);
+        System.out.println();
+        userAccountService.createCustomerAccount(customerAccount);
+        return "redirect:/customer-register";
     }
 
 }
