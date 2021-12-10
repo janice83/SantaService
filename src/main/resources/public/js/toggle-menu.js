10f73ebb206c7b3ce586dae5e27ab2000f40ac38
SJarno <jarno.saastamoinen@hotmail.com>
2021-12-10 19:38:55 +0200

    fix issue with images in profiles

--- src/main/resources/public/js/toggle-menu.js
+++ src/main/resources/public/js/toggle-menu.js
@@ -1,8 +1,8 @@
 function toggleMobileMenu() {
     let navLinks = document.getElementById("navLinks");
-    if (navLinks.style.display === "flex") {
+    if (navLinks.style.display === "block") {
         navLinks.style.display = "none";
     } else {
-        navLinks.style.display = "flex";
+        navLinks.style.display = "block";
     }
 }
